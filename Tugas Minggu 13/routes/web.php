90968aa0cb1b00960008a56dc8424ca6d2522393
BelajarLaravel <ariniddianaoktavia@gmail.com>
2021-06-10 22:58:55 +0700

    Tugas Minggu 13

---
+++ Tugas Minggu 13/routes/web.php
@@ -0,0 +1,80 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ArinidController;
+// use App\Http\Controllers\HomeController;
+// use App\Http\Controllers\Backend\DashboardController;
+// use App\Http\Controllers\Backend\PendidikanController;
+//use Illuminate\Support\Facades\Auth;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+/*Route::get('/hello', function () {
+    return 'Hello world';
+});
+Route::get('/belajar', function () {
+    echo '<h1>Hello World</h1>';
+    echo '<p>Sedang belajar Laravel</p>';
+});
+Route::get('page/{nomor}', function($nomor){
+    return 'Ini Halaman ke-' . $nomor;
+});
+Route::get('/gambar', function () {
+    return view('gambar');
+});*/
+
+/*Route::get('user', [HomeController::class, 'index']);
+
+Route::resource('user', HomeController::class);
+
+Route::get("/Arinid", function(){
+    return view("Arinid");
+});
+
+Route::group(['namespace'=> 'frontend'], function(){
+	Route::resource('home', 'HomeController');
+});
+
+Route::group(['namespace'=>'backend'], function()
+{
+  Route::resource('admin','DashboardController');
+});*/
+
+/*Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::group(['middleware' => ['web','auth']], function () {
+    Route::group(['namespace' => 'Backend'], function()
+    {
+        Route::resource('dashboard', DashboardController::class);
+    });
+});*/
+
+Route::resource('home', HomeController::class);
+
+// Route::resource('dashboard', DashboardController::class);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+// Route::resource('pendidikan', PendidikanController::class);
+
+Route::group(['middleware' => ['web','auth']], function () {
+	Route::group(['namespace' => 'Backend'], function()
+	{
+		Route::resource('dashboard', DashboardController::class);
+		Route::resource('pendidikan', PendidikanController::class);
+	});
+});
